a7bc642d53a1a0f24392116bf613cdebc433735c
Mo Mahboobian <momahboobian@gmail.com>
2024-09-19 13:00:51 +0100

    Update header and more styling

--- focus-mate/src/components/Header.js
+++ focus-mate/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import {
   AppBar,
   Toolbar,
@@ -8,18 +8,19 @@ import {
   Dialog,
   DialogTitle,
   DialogContent,
-  DialogContentText,
   DialogActions,
+  Card,
+  CardContent,
+  CardMedia,
+  IconButton,
 } from '@mui/material'
-import axios from 'axios'
+import { Close as CloseIcon } from '@mui/icons-material'
 import Person from './Person'
 
-const Header = ({ data }) => {
-  const [person, setPerson] = useState(null)
+const Header = ({ person }) => {
   const [open, setOpen] = useState(false)
 
-  const handleAvatarClick = person => {
-    setPerson(person)
+  const handleAvatarClick = () => {
     setOpen(true)
   }
 
@@ -27,24 +28,9 @@ const Header = ({ data }) => {
     setOpen(false)
   }
 
-  useEffect(() => {
-    fetchRandomPerson()
-  }, [])
-
-  const fetchRandomPerson = async () => {
-    try {
-      const response = await axios.get('/api/persons')
-      const persons = response.data
-      const randomPerson = persons[Math.floor(Math.random() * persons.length)]
-      setPerson(randomPerson)
-    } catch (error) {
-      console.log('An error occurred while fetching data:', error)
-    }
-  }
-
   return (
     <div>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ mb: 2 }}>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             BBC E2E
@@ -55,22 +41,37 @@ const Header = ({ data }) => {
             <Avatar
               alt={person.name}
               src={person.photo}
-              onClick={() => handleAvatarClick(person)}
+              onClick={handleAvatarClick}
+              sx={{ cursor: 'pointer', transition: '0.3s', '&:hover': { transform: 'scale(1.1)' } }}
             />
           )}
         </Toolbar>
       </AppBar>
 
       {person && (
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>{person.name}</DialogTitle>
+        <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
+          <DialogTitle>
+            <IconButton
+              edge="end"
+              color="inherit"
+              onClick={handleClose}
+              sx={{ position: 'absolute', right: 8, top: 8 }}
+            >
+              <CloseIcon />
+            </IconButton>
+          </DialogTitle>
           <DialogContent>
-            <DialogContentText>
-              <Person person={person} />
-            </DialogContentText>
+            <Card>
+              <CardMedia component="img" height="140" image={person.photo} alt={person.name} />
+              <CardContent>
+                <Person person={person} />
+              </CardContent>
+            </Card>
           </DialogContent>
           <DialogActions>
-            <Button onClick={handleClose}>Close</Button>
+            <Button onClick={handleClose} color="primary">
+              Close
+            </Button>
           </DialogActions>
         </Dialog>
       )}
